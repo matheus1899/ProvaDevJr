10c504b396e8ddb3cfc48b72c9195ad66af6ca7f
Matheus Tenorio <matheus.santos1899@gmail.com>
2021-01-05 08:41:23 -0300

    Tarefa 1 concluída

--- Tarefa1.cs
+++ Tarefa1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace ProvaDevJr
@@ -8,8 +7,63 @@ namespace ProvaDevJr
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> listaOriginal = new List<int>() { 7, 5, 3, 9, 6, 4, 1 };
+            List<int> listaNova = new List<int>();
+            for (int i = 0; i < listaOriginal.Count; i++)
+            {
+                if (listaOriginal[i] == 4)
+                {
+                    continue;
+                }
+                else if (listaOriginal[i] == 9)
+                {
+                    listaNova.Add(5);
+                    continue;
+                }
+                else
+                {
+                    listaNova.Add(listaOriginal[i]);
+                }
+            }
+            exibeLista(listaOriginal, "Original");
+            exibeListaESomatoria(listaNova, "Nova    ");
             Console.ReadKey();
+        }
+        static void exibeLista(List<int> lista, string nomeLista)
+        {
+            int tamanho = lista.Count;
+
+            Console.Write(nomeLista + ": ");
+            for (int i = 0; i < tamanho; i++)
+            {
+                if (i == tamanho - 1)
+                {
+                    Console.WriteLine(lista[i]);
+                }
+                else
+                {
+                    Console.Write(lista[i] + ", ");
+                }
+            }
+        }
+        static void exibeListaESomatoria(List<int> lista, string nomeLista)
+        {
+            int tamanho = lista.Count;
+            int soma = 0;
+            Console.Write(nomeLista + ": ");
+            for (int i = 0; i < tamanho; i++)
+            {
+                soma += lista[i];
+                if (i == tamanho - 1)
+                {
+                    Console.WriteLine(lista[i]);
+                }
+                else
+                {
+                    Console.Write(lista[i] + ", ");
+                }
+            }
+            Console.WriteLine("Somatoria: " + soma);
         }
     }
 }
