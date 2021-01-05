7da42a48fb5b3f5430f60687ff1f4e3b9fffeaf2
Matheus Tenorio <matheus.santos1899@gmail.com>
2021-01-05 08:39:13 -0300

    Adicionar arquivos de projeto.

---
+++ Tarefa1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ProvaDevJr
+{
+    class Tarefa1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
